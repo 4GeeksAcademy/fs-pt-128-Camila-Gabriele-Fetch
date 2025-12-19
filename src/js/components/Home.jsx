81dcbe732d0338f17f7247e8b4a4549e93d5e9e9
Camila Gabriele <camilagabriele@outlook.com.ar>
2025-12-19 20:31:27 +0000

    cambios

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,27 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
+
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+	const url = 'https://playground.4geeks.com/todo/'
+
+	const [listaTareas, setListaTareas] = useState([])
+	
+	const leerTareas = async () => {
+		const respuesta = await fetch(`${url}/users/Camila`)
+		console.log(respuesta);
+		if (!respuesta.ok){
+			console.log("Tarea no existente, crea una nueva");
+			crearTarea ()
+			return	
+		}
+		const data = await respuesta.json()
+		console.log(data);	
+	}
+
+	const crearTarea = async () => {
+		const respuesta = await fetch(`${url}/users/Camila` , {
+			method: "POST"
+		})
+		console.log(respuesta);
+		const data = await respuesta.json()
+		console.log(data);
+	}
+
+	return (
+		<>
+			<div class="rounded" >
+				<h3 className="text-center mt-3"><strong> ¿Que necesito? </strong></h3>
+				<div className="input-group p-3">
+					<input type="text" className="form-control" placeholder="Lo necesario . . ."/>
+					<button className="btn btn-ligth border border-dark"> Agregar 🛒</button>
+				</div>
+			</div>
+		</>
 	);
 };
 
